8033c761799806429c3c7fb8448eca5e36b6633d
StrongerProgrammer7 <96116317+DM-DOOMEnternal@users.noreply.github.com>
2022-02-28 15:03:47 +0300

    Task12&13
    12:Предыдущую программу реализовать в функции main с
    помощью только оператора суперпозиции, потом только с помощью
    оператора каррирования.
    13: Написать функции произведение цифр числа, минимальная и
    максимальная цифра с использованием рекурсии вверх и с
    использованием хвостовой рекурсии.

---
+++ Task12/Task12/Program.fs
@@ -0,0 +1,20 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+(*Задание 12 Предыдущую программу реализовать в функции main с
+помощью только оператора суперпозиции, потом только с помощью
+оператора каррирования.*)
+
+let answerUser answer =
+    if(answer="F#" || answer ="Prolog") then  printfn "you loser!"
+    else printfn "You cool!!"
+
+[<EntryPoint>]
+let main argv =
+    printfn "You favorite high-level programming?"
+    let answer = System.Console.ReadLine()
+    printf "Карирование: " 
+    answer |> answerUser
+    printfn "Суперпозиция %s" (if(answer="F#" || answer ="Prolog") then  "you loser!"
+                                else "You cool!!")
+    0
---
+++ Task13/Task13/Program.fs
@@ -0,0 +1,39 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+(*Задание 13 Написать функции произведение цифр числа, минимальная и
+максимальная цифра с использованием рекурсии вверх и с
+использованием хвостовой рекурсии.*)
+
+(*Рекурсия вверх*)
+let rec recUpMult number =
+    if (number = 0) then 1
+    else (number%10)*(recUpMult (number/10))
+
+let rec recUpMax number max =
+    if (number = 0) then max
+    else if (number%10>max) then recUpMax(number/10)(number%10)
+    else recUpMax(number/10) max
+   
+
+let rec recUpMin number min =
+    if (number = 0) then min
+    else if (number%10<min) then recUpMin(number/10)(number%10)
+    else recUpMin(number/10) min
+
+(*Рекурсия вниз или хвостовая рекурсия*)
+let rec recDownMult number mult =
+    if (number = 0) then mult
+    else recDownMult (number/10) (mult*(number%10))
+
+let linkRecDownMult number = recDownMult number 1
+
+
+[<EntryPoint>]
+let main argv =
+    printfn "Произведение цифр: %d" (recUpMult 123)
+    printfn "Макс цифра: %d" (recUpMax 173 0)
+    printfn "Мин цифра: %d" (recUpMin 123 3)
+    printfn "Рекурсия вниз"
+    printfn "Произведение цифр: %d" (linkRecDownMult 123)
+    0 // return an integer exit code
